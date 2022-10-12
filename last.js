1ac9ad224d4b1227a0f8479013e51ce034fff098
Ishika Gupta <113205098+IshikaGupta3112@users.noreply.github.com>
2022-10-12 15:05:34 +0530

    js for game over page

---
+++ last.js
@@ -0,0 +1,8 @@
+setTimeout(()=>{
+    document.getElementById('brain').style.visibility="visible";
+} , 3500);
+setTimeout(()=>{
+    document.getElementById('btndiv').style.visibility="visible";
+} , 4000);
+
+  
